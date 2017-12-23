af5a87087a6d3609295e0f9094f4e2a08383850b
Oliver Egger <oliver.egger@ahdis.ch>
2017-12-23 15:52:13 +0100

    using native fhir adapter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
+/// <reference path="../types/fhir.js/src/adapters/native/index.d.ts" />
+/// <reference path="../types/fhir.js/index.d.ts" />
 import { Component } from '@angular/core';
+import { IFhir, Config, ResponseObj, Entry, IResource, ResourceType, ReadObj, VReadObj } from 'fhir.js';
+import fhirAdapter = require('fhir.js/src/adapters/native');
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  private client: IFhir;
+
+  private config: Config = {
+           baseUrl: 'http://fhirtest.uhn.ca/baseDstu3',
+    // baseUrl: 'http://localhost:8080/baseDstu3',
+    credentials: 'same-origin'
+  };
+
+  title: 'ng-fhir-sample';
+  clickMessage: string;
+  fhirOutput: string;
+
+  constructor() {
+    this.client = fhirAdapter(this.config);
+    this.clickMessage = '';
+    this.fhirOutput = '';
+}
+
+  async testCreate() {
+    try {
+
+      console.log('Creating a the patient');
+      const entry: Entry = {
+          resource: {
+            resourceType: 'Patient'
+          }
+        };
+      entry.debug = true;
+      const response = await this.client.create(entry);
+      console.log(JSON.stringify(response));
+      this.fhirOutput += ' success ' + JSON.stringify(response);
+    } catch (error) {
+      console.log('error');
+      console.log(JSON.stringify(error));
+      this.fhirOutput += ' error ' + JSON.stringify(error);
+    }
+
+  }
+
+  onClickMe() {
+    this.testCreate();
+    this.clickMessage = 'testCreate called';
+  }
+
 }
