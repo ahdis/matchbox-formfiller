e87800970147e791002051b9fe811893fe024fd3
Oliver Egger <oliver.egger@ahdis.ch>
2018-01-08 23:47:47 +0100

    providing FhirConfig from app.module instead of ng-fhirjs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,12 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { NgFhirjsModule } from './modules/ng-fhirjs/ng-fhirjs.module';
+import { FhirJsHttpService, FHIR_HTTP_CONFIG } from './modules/ng-fhirjs/fhir-js-http.service';
 
+export const FHIR_JS_CONFIG: FhirConfig = {
+  baseUrl: 'http://localhost:8080/baseDstu3',
+  credentials: 'same-origin'
+};
 
 @NgModule({
   declarations: [
@@ -15,7 +20,7 @@ import { NgFhirjsModule } from './modules/ng-fhirjs/ng-fhirjs.module';
     HttpClientModule,
     NgFhirjsModule
   ],
-//  providers: [FhirJsHttpService, { provide: FHIR_HTTP_CONFIG, useValue: FHIR_JS_CONFIG}],
+  providers: [{ provide: FHIR_HTTP_CONFIG, useValue: FHIR_JS_CONFIG}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
