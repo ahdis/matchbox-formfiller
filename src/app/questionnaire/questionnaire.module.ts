23a35feb7cb4944fc0add4a0a5dc7ca8be90b41c
Moritz Habegger <24765170+moritzhabegger@users.noreply.github.com>
2019-05-24 08:15:22 +0200

    generate labels for questionnaire-supportLink (#7)
    * generate labels for questionnaire-supportLink
    
    * add findings from review

--- src/app/questionnaire/questionnaire.module.ts
+++ src/app/questionnaire/questionnaire.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { ItemLabelComponent } from './item-label/item-label.component';
 import { SharedModule } from '../shared/shared.module';
+import { ItemHintComponent } from './item-hint/item-hint.component';
 
-const exportedComponents = [ItemLabelComponent];
+const exportedComponents = [ItemLabelComponent, ItemHintComponent];
 
 @NgModule({
   imports: [SharedModule],
