fe94cffcd56cb4324b0b4b1f68d74a8074dc4ff1
Oliver Egger <oliver.egger@ahdis.ch>
2021-04-02 18:15:54 +0200

    prepare for qr response

--- src/app/home/questionnaire-demo.ts
+++ src/app/home/questionnaire-demo.ts
@@ -1,4 +1,5 @@
 import * as questionnaireEbida from '../../examples/ebida-order-1.json';
+import * as questionnaireEbidaQr from '../../examples/ebida-order-1-qr.json';
 import * as questionnaireReferralMin from '../../examples/referral-min.json';
 import * as questionnaireSdcCap from '../../examples/Questionnaire-questionnaire-sdc-profile-example-cap.json';
 import * as questionnaireSdcLoinc from '../../examples/Questionnaire-questionnaire-sdc-profile-example-loinc.json';
@@ -10,6 +11,10 @@ import * as questionnaireSupportLink from '../../examples/support-link-questionn
 export class QuestionnaireDemo {
   static getQuestionnaireEbida(): fhir.r4.Questionnaire {
     return <fhir.r4.Questionnaire>(<any>questionnaireEbida);
+  }
+
+  static getQuestionnaireEbidaQr(): fhir.r4.QuestionnaireResponse {
+    return <fhir.r4.QuestionnaireResponse>(<any>questionnaireEbidaQr);
   }
 
   static getQuestionnaireReferralMin(): fhir.r4.Questionnaire {
--- src/app/questionnaire-detail/questionnaire-detail.component.ts
+++ src/app/questionnaire-detail/questionnaire-detail.component.ts
@@ -19,6 +19,6 @@ export class QuestionnaireDetailComponent implements OnInit {
 
   fillForm() {
     this.questionaireFillerServer.setQuestionnare(this.questionnaire);
-    this.router.navigate(['/questionnaire-form-filler']);
+    this.router.navigate(['questionnaire', '-1']);
   }
 }
