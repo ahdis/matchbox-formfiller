aa28e8a0be6106f32cfa197afb1821eb4bfd739a
Oliver Egger <oliver.egger@ahdis.ch>
2021-05-18 11:51:16 +0200

    update client

--- src/app/client/client.component.ts
+++ src/app/client/client.component.ts
@@ -11,6 +11,8 @@ import debug from 'debug';
 })
 export class ClientComponent implements OnInit {
   public map: FormControl;
+  public resourceType: FormControl;
+  public id: FormControl;
 
   client: FhirClient;
   static log = debug('app:');
@@ -19,11 +21,47 @@ export class ClientComponent implements OnInit {
   constructor(private cd: ChangeDetectorRef, private data: FhirConfigService) {
     this.client = data.getFhirClient();
     this.map = new FormControl();
+    this.resourceType = new FormControl();
+    this.id = new FormControl();
   }
 
   ngOnInit() {}
 
-  onSubmit() {
+  onGet() {
+    ClientComponent.log('onGet');
+    this.client
+      .read({
+        resourceType: this.resourceType.value,
+        id: this.id.value,
+      })
+      .then((response) => {
+        this.map.setValue(JSON.stringify(response));
+      })
+      .catch((error) => {
+        this.errMsg = 'Error' + error;
+      });
+  }
+
+  onUpdate() {
+    ClientComponent.log('onUpdate ');
+    let res: fhir.r4.Resource;
+    res = JSON.parse(this.map.value);
+
+    this.client
+      .update({
+        resourceType: res.resourceType,
+        id: res.id,
+        body: res,
+      })
+      .then((response) => {
+        this.map.setValue(JSON.stringify(response));
+      })
+      .catch((error) => {
+        this.errMsg = 'Error' + error;
+      });
+  }
+
+  onPost() {
     ClientComponent.log('onSubmit ');
     let res: fhir.r4.Resource;
     res = JSON.parse(this.map.value);
